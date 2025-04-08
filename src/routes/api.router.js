e150dd2b70a4564f34e272a71de154f8355fa297
Dangelo Godoy <dangelogodoy@gmail.com>
2025-04-07 22:30:41 -0400

    config of router for cookies, sessions, users and auth

--- src/routes/api.router.js
+++ src/routes/api.router.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import usersRouter from "./api/users.router.js";
+import cookieRouter from "./api/cookies.router.js";
+import sessionsRouter from "./api/sessions.router.js";
+import authRouter from "./api/auth.router.js";
 
 const apiRouter = Router();
+
+apiRouter.use("/cookies", cookieRouter);
+apiRouter.use("/sessions", sessionsRouter);
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/auth", authRouter);
 
 export default apiRouter;
