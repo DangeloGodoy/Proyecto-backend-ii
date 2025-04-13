ac5c76570459937dad491da6aa2ea8fd8ddd918c
DangeloGodoy <56121623+DangeloGodoy@users.noreply.github.com>
2025-04-13 01:16:59 +0000

    fix: improve error handling and formatting in login script

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,24 +1,24 @@
 document.querySelector("#login").addEventListener("click", async () => {
-    try {
-        const data = {
-            email: document.querySelector("#email").value,
-            password: document.querySelector("#password").value
-        }
-        const opts = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-        }
-        const url = "/api/auth/login";
-        let response = await fetch(url, opts);
-        response = await response.json();
-        if (response.error) {
-            alert(response.error);
-        } else {
-            location.replace("/");
-        }
-    } catch (error) {
-        //modificar a tostyfiay
-        alert("Error al registrar el usuario");
+  try {
+    const data = {
+      email: document.querySelector("#email").value,
+      password: document.querySelector("#password").value,
+    };
+    const opts = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+    const url = "/api/auth/login";
+    let response = await fetch(url, opts);
+    response = await response.json();
+    if (response.error) {
+      alert(response.error);
+    } else {
+      location.replace("/");
     }
-})
+  } catch (error) {
+    //modificar a tostyfiay
+    alert("Error al registrar el usuario");
+  }
+});
