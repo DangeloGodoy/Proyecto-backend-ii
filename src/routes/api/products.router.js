411b2e5e27972c4e796dd13660d502894f146fbc
DangeloGodoy <56121623+DangeloGodoy@users.noreply.github.com>
2025-04-13 01:51:43 +0000

    feat: refactor product routes to use passport callback and improve async handling

--- src/routes/api/products.router.js
+++ src/routes/api/products.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import passport from "../../middlewares/passport.mid.js";
 import { productsManager } from "../../data/mongo/managers/manager.mongo.js";
+import { passportCb } from "../../middlewares/passportCb.mid.js";
+import { Types } from "mongoose";
 
 const productsRouter = Router();
 
@@ -17,56 +18,77 @@ const createOne = async (req, res, next) => {
     next(error);
   }
 };
-const readAll = (req, res, next) => {
+const readAll = async (req, res, next) => {
   try {
+    const response = await productsManager.readAll();
+    res.status(200).json({
+      response,
+      method: req.method,
+      url: req.originalUrl,
+    });
   } catch (error) {
     next(error);
   }
 };
-const readById = (req, res, next) => {
+const readById = async (req, res, next) => {
   try {
+    const data = req.params;
+    const response = await productsManager.readById(data.pid);
+    res.status(200).json({
+      response,
+      method: req.method,
+      url: req.originalUrl,
+    });
   } catch (error) {
     next(error);
   }
 };
-const updateById = (req, res, next) => {
+const updateById = async (req, res, next) => {
   try {
+    const data = req.params;
+    const updateData = req.body;
+    const response = await productsManager.updateById(data.pid, updateData);
+    res.status(200).json({
+      response,
+      method: req.method,
+      url: req.originalUrl,
+    });
   } catch (error) {
     next(error);
   }
 };
-const deleteById = (req, res, next) => {
+const deleteById = async (req, res, next) => {
   try {
+    const data = req.params;
+    const response = await productsManager.deleteById(data.pid);
+    res.status(200).json({
+      response,
+      method: req.method,
+      url: req.originalUrl,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const pidParam = async (req, res, next, pid) => {
+  try {
+    const isObjectId = Types.ObjectId.isValid(pid);
+    if (!isObjectId) {
+      const error = new Error("Invalid product ID");
+      error.statusCode = 400;
+      throw error;
+    }
+    return next();
   } catch (error) {
     next(error);
   }
 };
 
-productsRouter.post(
-  "/",
-  passport.authenticate("admin", {
-    session: false,
-    failureRedirect: "/api/auth/bad-auth",
-  }),
-  createOne
-);
+productsRouter.param("pid", pidParam);
+productsRouter.post("/", passportCb("admin"), createOne);
 productsRouter.get("/", readAll);
-productsRouter.get("/:uid", readById);
-productsRouter.put(
-  "/:uid",
-  passport.authenticate("admin", {
-    session: true,
-    failureRedirect: "/api/auth/bad-auth",
-  }),
-  updateById
-);
-productsRouter.delete(
-  "/uid",
-  passport.authenticate("admin", {
-    session: true,
-    failureRedirect: "/api/auth/bad-auth",
-  }),
-  deleteById
-);
+productsRouter.get("/:pid", readById);
+productsRouter.put("/:pid", passportCb("admin"), updateById);
+productsRouter.delete("/pid", passportCb("admin"), deleteById);
 
 export default productsRouter;
