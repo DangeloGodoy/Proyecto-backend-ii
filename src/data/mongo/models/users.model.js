f86e2d1b8d18cb1e2e292ee0a03d08c0c2cd24d4
DangeloGodoy <56121623+DangeloGodoy@users.noreply.github.com>
2025-04-13 01:16:46 +0000

    refactor: update user model schema to make city field required

--- src/data/mongo/models/users.model.js
+++ src/data/mongo/models/users.model.js
@@ -2,15 +2,20 @@ import { Schema, model } from "mongoose";
 
 const collection = "users";
 const schema = new Schema(
-    {
-        name: { type: String },
-        date: { type: Date, default: Date.now },
-        city: { type: String },
-        email: { type: String, required: true, unique: true, index: true },
-        password: { type: String, required: true },
-        role: { type: String, default: "USER", enum: ["USER", "ADMIN"] },
-        avatar: { type: String, default: "https://www.svgrepo.com/show/350417/user-circle.svg" },
-    }, { timestamps: true });
+  {
+    name: { type: String },
+    date: { type: Date, default: Date.now },
+    city: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "USER", enum: ["USER", "ADMIN"] },
+    avatar: {
+      type: String,
+      default: "https://www.svgrepo.com/show/350417/user-circle.svg",
+    },
+  },
+  { timestamps: true }
+);
 
-    const User = model(collection, schema);
-    export default User;
+const User = model(collection, schema);
+export default User;
