d66894b9fe6ebce76b1b81a550436228b5d433ff
DangeloGodoy <56121623+DangeloGodoy@users.noreply.github.com>
2025-04-14 04:36:19 +0000

    fix: update fetch method to GET for online status and improve error handling in navbar script

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,42 +1,41 @@
 const isOnline = async () => {
-    try {
-        const opts = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-        };
-        const url = "/api/auth/online";
-        let response = await fetch(url, opts);
-        response = await response.json();
-        const selector = document.querySelector("#opts");
-        if (response.online) {
-            console.log(response);            
-            selector.innerHTML = `
+  try {
+    const opts = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    const url = "/api/auth/online";
+    let response = await fetch(url, opts);
+    response = await response.json();
+    const selector = document.querySelector("#opts");
+    if (response.user_id) {
+      selector.innerHTML = `
             <a class="btn btn-success py-1 px-2 m-1" href="/profile/${response.user_id}">Profile</a>
             <a class="btn btn-success py-1 px-2 m-1" href="/cart/${response.user_id}">Cart</a>
             <button class="btn btn-succes py-1 px-2 m-1" id="signout">Logout</button>
             `;
-            document.querySelector("#signout").addEventListener("click", async() => {
-                try {
-                    const opts = {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                    };
-                    const url = "api/auth/signout";
-                    await fetch(url, opts);
-                    window.location.href = "/";
-                } catch (error) {
-                    console.error(error);
-                }
-            })
-        } else {
-            selector.innerHTML = `
+      document.querySelector("#signout").addEventListener("click", async () => {
+        try {
+          const opts = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+          };
+          const url = "api/auth/signout";
+          await fetch(url, opts);
+          window.location.href = "/";
+        } catch (error) {
+          console.error(error);
+        }
+      });
+    } else {
+      selector.innerHTML = `
             <a class="btn btn-success py-1 px-2 m-1" href="/login">Login</a>
             <a class="btn btn-success py-1 px-2 m-1" href="/register">Register</a>
             `;
-        }
-    } catch (error) {
-        console.error(error);
     }
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 isOnline();
