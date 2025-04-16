7b74bfa111bae9d82f74c52f028259b778257dc0
Dangelo Godoy <dangelogodoy@gmail.com>
2025-04-16 00:57:36 -0400

    fix: update user schema to use camelCase for firstName and lastName fields

--- src/data/mongo/models/users.model.js
+++ src/data/mongo/models/users.model.js
@@ -3,9 +3,9 @@ import { Schema, model } from "mongoose";
 const collection = "users";
 const schema = new Schema(
   {
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    age: { type: Number},
+    firstName: { type: String }, 
+    lastName: { type: String },
+    age: { type: Number },
     email: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: true },
     role: { type: String, default: "USER", enum: ["USER", "ADMIN"] },
