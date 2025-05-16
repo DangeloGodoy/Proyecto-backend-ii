e8c1a6ab9a574cef939f094447eec9586c299335
Dangelo Godoy <dangelogodoy@gmail.com>
2025-05-16 15:44:49 -0400

    fix: update server configuration to use environment variables and improve logging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import "dotenv/config.js";
+import "./src/helpers/setEnv.helper.js"
 import express from "express";
 import { engine } from "express-handlebars";
 import cookieParser from "cookie-parser";
@@ -11,12 +11,14 @@ import DatabaseConect from "./src/helpers/dbConnect.helper.js";
 import router from "./src/routes/index.router.js";
 import { errorHandler } from "./src/middlewares/errorHandler.mid.js";
 import { pathHandler } from "./src/middlewares/pathHandler.mid.js";
+import args from "./src/helpers/setArgs.helper.js";
 
 /* Server */
 const server = express();
-const port = 8080;
+const port = process.env.PORT;
 const ready = () => {
   console.log(`Server is running on http://localhost:${port}`);
+  console.log("Server ready on mode " + args.mode);
   const db = new DatabaseConect(process.env.MONGO_URL);
   db.dbConnect(process.env.MONGO_URL);
 };
@@ -33,10 +35,12 @@ server.use(cookieParser(process.env.COOKIE_SECRET));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
-server.use(cors({
-  origin: true,
-  credentials: true,
-}));
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 server.use(
   sessions({
